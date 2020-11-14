3010676b0c875cb1e5e2322fb66bfeb27332b1a9
andremarqueda <andremarqueda1@gmail.com>
2020-11-13 19:49:30 -0600

    Ver 1.0
    Successfully describes the steps to solve the puzzle

--- AI_Puzzle.py
+++ AI_Puzzle.py
@@ -8,7 +8,7 @@ No dependencies needed
 
 INITIAL_STATE=[0,0,0,5,4] # Los ceros representan un espacio aún no ocupado
 CONSTANTES=[1,2,3]# Números a colocar en las casillas desocupadas
-
+rutaSol=[]
 class Node:
 
     def __init__(self, data):#Declaración del nodo
@@ -24,18 +24,7 @@ class Node:
         self.hijos.append(node)
         node.padre=self
     #Imprime el estatus general dado un nodo
-    def showNodeStatus(self):
-        print("El estado del nodo actual es")
-        self.printValue()
-        print("#hijos: ",len(self.hijos))
-        if self.padre !=None:
-            print("padre: ",self.padre.printValue())
-        else:
-            print("Nodo Raíz")
     
-    def recorrerArbol(self):
-        for i in range(len(self.hijos)):
-            self.hijos[i].printValue()
 
 
 
@@ -55,37 +44,41 @@ def operadorRaiz(nodo):
                 if estadoActual[j]==0:#Si la casilla está vacía
                     estadoActual[j]=operadoresDisponibles[i]#Entonces se aplica el operador
                     hijo=Node(estadoActual)#Se crea el nuevo nodo, con el operador aplicado
+                    
                     nodo.insertNode(hijo)#Se inserta el nodo como hijo
+                    
                 else:
                     pass
         for i in range(len(nodo.hijos)):
             operadorRaiz(nodo.hijos[i])#Repetir este procedimiento para cada uno de los hijos
             
-            """
-            Implementar BFS para verificar la integridad del arbol 
-            Finalmente implementar lo de la ruta solución
- 
- 
-            """
-def BFS(nodo,capa):
-    nodo.printValue()
-    sumaVertical=nodo.data[0]+nodo.data[2]+nodo.data[4]
-    sumaHorizontal=nodo.data[1]+nodo.data[2]+nodo.data[3]
-    if sumaHorizontal==9 and sumaVertical==9:
-        print("SOLUCIÓN ENCONTRADA: ",nodo.data)
         
-    else:
-        for i in range (len(nodo.hijos)):
-            if len(nodo.hijos)!=0:
-                nodo.hijos[i].printValue()
-                
-            else:
-                print("Hoja")
-        for i in range(len(nodo.hijos)):
-            BFS(nodo.hijos[i],capa+1)
+def BFS(nodo,flag):
+    if flag==False: #Si aún no se ha encontrado la solución, continuar iterando
+        sumaVertical=nodo.data[0]+nodo.data[2]+nodo.data[4]#Comprobar que se cumpla la suma vertical del nodo actual
+        sumaHorizontal=nodo.data[1]+nodo.data[2]+nodo.data[3]#Comprobar que se cumpla la suma horizontal del nodo actual
+        if sumaHorizontal==9 and sumaVertical==9:#Si se cumplen ambas
+            flag=True#No se itera más y se realiza la impresión recursiva de la ruta solución
+            global rutaSol
+            rutaSol=nodo.data[:]
+            return nodo.data
+        else:#En caso contrario
+            for i in range (len(nodo.hijos)):#Seguir recorriendo los hijos
+                if len(nodo.hijos)!=0:#Si el nodo no es una hoja
+                    nodo.hijos[i].printValue()#imprimir su valor
+            for i in range(len(nodo.hijos)): 
+                    if flag==True:#Si aun no se ha encontrado una solución
+                        pass
+                    else:
+                        BFS(nodo.hijos[i],flag=False) #Continuar recursivamente analizando los hijos siempre y cuando no sea una hoja, en ese caso
+                                                    #Regresar un nivel y analaizar los nodos hermanos
+
         
     
-    
+def rutaSolucion(estadoSolu):
+    global INITIAL_STATE
+    for i in range(len(INITIAL_STATE)-2):
+        print("Colocar ", estadoSolu[i]," En la casilla",i)
 
 print ("Iniciando creación del árbol")
 Raiz=Node(INITIAL_STATE)
@@ -93,4 +86,8 @@ print ("Aplicando operadores y realizando bitácora de desarrollo:")
 operadorRaiz(Raiz)
 print ("Buscando ruta solución:")
 BFS(Raiz,0)
+print("SOLUCIÓN ENCONTRADA, DETENIENDO PROCESO:\n",rutaSol)
+rutaSolucion(rutaSol)
+
+
 
